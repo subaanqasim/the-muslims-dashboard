45226e2d1c6fa484371fde28192267d2604cd816
Subaan Qasim <96848399+subaanqasim@users.noreply.github.com>
2022-04-13 10:02:56 +0100

    added shortcut display under theme icon

--- src/components/ToggleThemeButton.tsx
+++ src/components/ToggleThemeButton.tsx
@@ -1,20 +1,33 @@
 import React from "react"
-import { ActionIcon, useMantineColorScheme } from "@mantine/core"
+import { ActionIcon, Kbd, useMantineColorScheme } from "@mantine/core"
 import { Sun, MoonStars } from "tabler-icons-react"
+import { useOs } from "@mantine/hooks"
 
 function ToggleThemeButton() {
     const { colorScheme, toggleColorScheme } = useMantineColorScheme()
     const dark = colorScheme === "dark"
+    const os = useOs()
 
     return (
-        <ActionIcon
-            variant="hover"
-            color={dark ? "yellow" : "blue"}
-            onClick={() => toggleColorScheme()}
-            title="Toggle color scheme"
+        <div
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+            }}
         >
-            {dark ? <Sun size={18} /> : <MoonStars size={18} />}
-        </ActionIcon>
+            <ActionIcon
+                variant="hover"
+                color={dark ? "yellow" : "blue"}
+                onClick={() => toggleColorScheme()}
+                title="Toggle color scheme"
+            >
+                {dark ? <Sun size={18} /> : <MoonStars size={18} />}
+            </ActionIcon>
+            <div>
+                <Kbd>{os === "macos" ? "⌘" : "Ctrl"}</Kbd> + <Kbd>J</Kbd>
+            </div>
+        </div>
     )
 }
 
